ea20658c40781d3d88a01a84ec5b5393f9a92a42
TA <tamilarasuva444@gmail.com>
2021-04-08 21:33:32 +0530

    sql added

--- database/physio.sql
+++ database/physio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2021 at 06:41 PM
+-- Generation Time: Apr 08, 2021 at 06:02 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.3.22
 
@@ -44,11 +44,35 @@ INSERT INTO `admin` (`id`, `email`, `password`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `appoinment`
+--
+
+CREATE TABLE `appoinment` (
+  `id` int(5) NOT NULL,
+  `pname` varchar(25) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `phone` bigint(10) NOT NULL,
+  `rehab` int(10) NOT NULL DEFAULT 0,
+  `doctor` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `appoinment`
+--
+
+INSERT INTO `appoinment` (`id`, `pname`, `date`, `time`, `phone`, `rehab`, `doctor`) VALUES
+(1, 'Pooja', '2021-04-23', '11:00:00', 7744558899, 0, 'Subash');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `doctor`
 --
 
 CREATE TABLE `doctor` (
   `id` int(7) NOT NULL,
+  `name` varchar(25) NOT NULL,
   `email` varchar(25) NOT NULL,
   `password` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -57,8 +81,9 @@ CREATE TABLE `doctor` (
 -- Dumping data for table `doctor`
 --
 
-INSERT INTO `doctor` (`id`, `email`, `password`) VALUES
-(1, 'doctor1@gmail.com', 'doctor1');
+INSERT INTO `doctor` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Sanjeev', 'sanjeevmbbs@gmail.com', 'sanjeev'),
+(2, 'Subash', 'subash@gmail.com', 'subash');
 
 --
 -- Indexes for dumped tables
@@ -68,6 +93,12 @@ INSERT INTO `doctor` (`id`, `email`, `password`) VALUES
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `appoinment`
+--
+ALTER TABLE `appoinment`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -88,10 +119,16 @@ ALTER TABLE `admin`
   MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `appoinment`
+--
+ALTER TABLE `appoinment`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
